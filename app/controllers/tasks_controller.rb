76733f3f2e786c966d428730b30ac38c7b74449f
Alexia Sander <sander.alexia@gmail.com>
2023-07-01 13:18:39 +0900

    step 4 done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,9 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all # Task and not tasks because it's the model
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # enable user to display page where all tasks are listed
-  get 'tasks', to: 'tasks#index', as: 'tasks'
+  # READ INDEX enable user to display page where all tasks are listed
+  get 'tasks', to: 'tasks#index', as: :tasks
+  # READ SHOW enable user to display tasks details
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
