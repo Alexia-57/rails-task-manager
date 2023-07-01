232c8b4b1eff3a887cbcacac7db6a1ed28d173e2
Alexia Sander <sander.alexia@gmail.com>
2023-07-01 14:00:46 +0900

    step 5 done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,29 @@
 class TasksController < ApplicationController
+
+  # READ
   def index
     @tasks = Task.all # Task and not tasks because it's the model
   end
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  # CREATE
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
 
   # READ INDEX enable user to display page where all tasks are listed
   get 'tasks', to: 'tasks#index', as: :tasks
+  # CREATE a task - part 1
+  get 'tasks/new', to: 'tasks#new', as: :new_task
   # READ SHOW enable user to display tasks details
   get 'tasks/:id', to: 'tasks#show', as: :task
+  # CREATE a task - part 2
+  post 'tasks', to: 'tasks#create'
 end
